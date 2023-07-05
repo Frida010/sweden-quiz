57ef201e265759b0039a77e73d69071ba0232c22
Frida010 <>
2023-07-05 09:06:24 +0000

    add more questions to the quiz to script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,50 @@ const questions = [
             { text: "Söderköping", correct: false },
         ]
     },
-    
-
+    {
+        question: "How much of Sweden's surface is covered by trees?",
+        answers: [
+            { text: "1/3", correct: false },
+            { text: "2/7", correct: false },
+            { text: "2/3", correct: true },
+            { text: "4/8", correct: false },
+        ]
+    },
+    {
+        question: "What is the currency of Sweden?",
+        answers: [
+            { text: "Euro", correct: false },
+            { text: "Swedish Pesos (SEP)", correct: false },
+            { text: "Swedish Crowns (SEK)", correct: true },
+            { text: "Swedish Dollar (SED)", correct: false },
+        ]
+    },
+    {
+        question: "What is the most sacred house rule in Sweden?",
+        answers: [
+            { text: "Take your shoes off indoors", correct: true },
+            { text: "No alcohol at home", correct: false },
+            { text: "No eating in-front of the TV", correct: false },
+            { text: "No pets allowed indoors", correct: false },
+        ]
+    },
+    {
+        question: "What is the coldest temperature ever recored in Sweden?",
+        answers: [
+            { text: "-56,6 Celsius", correct: false },
+            { text: "-42,6 Celsius ", correct: false },
+            { text: "-46,6 Celsius ", correct: false },
+            { text: "-52,6 Celsius", correct: true },
+        ]
+    },
+    {
+        question: "How many lakes are there in Sweden? (circa)",
+        answers: [
+            { text: "15 000", correct: false },
+            { text: "70 000", correct: false },
+            { text: "100 000", correct: true },
+            { text: "5000", correct: false },
+        ]
+    }
 
 ];
