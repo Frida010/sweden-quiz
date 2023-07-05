c91b9bf528dd09b9ecaad2f5f700d2ae3aeb7e58
Frida010 <>
2023-07-05 08:57:17 +0000

    add first question to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// Questions and answers
+
+const questions = [
+
+    {
+        question: "What is the name of Sweden's capital city?",
+        answers: [
+            { text: "Skellefteå", correct: false },
+            { text: "Stockholm", correct: true },
+            { text: "Sundsvall", correct: false },
+            { text: "Söderköping", correct: false },
+        ]
+    },
+    
+
+
+];
